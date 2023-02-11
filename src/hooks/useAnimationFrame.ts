c9935535884c9fabc4424cf08b9748b5091759ed
ryochansq <nicestsnsq@gmail.com>
2023-02-12 08:48:16 +0900

    test

--- src/hooks/useAnimationFrame.ts
+++ src/hooks/useAnimationFrame.ts
@@ -3,15 +3,12 @@ import { useCallback, useEffect, useRef } from "react";
 export const useAnimationFrame = (callback: (ms: number) => void) => {
   const reqIdRef = useRef(0);
   const timestamp = useRef<number>();
-  const loop: FrameRequestCallback = useCallback(
-    (nextTimestamp) => {
-      reqIdRef.current = requestAnimationFrame(loop);
-      const ms = timestamp.current ? nextTimestamp - timestamp.current : 0;
-      timestamp.current = nextTimestamp;
-      callback(ms);
-    },
-    [callback]
-  );
+  const loop: FrameRequestCallback = (nextTimestamp) => {
+    reqIdRef.current = requestAnimationFrame(loop);
+    const ms = timestamp.current ? nextTimestamp - timestamp.current : 0;
+    timestamp.current = nextTimestamp;
+    callback(ms);
+  };
 
   useEffect(() => {
     reqIdRef.current = requestAnimationFrame(loop);
