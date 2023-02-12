220914fc06d8935fe356631db907afe4c75f3041
ryochansq <nicestsnsq@gmail.com>
2023-02-12 13:54:00 +0900

    add total

--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -1,7 +1,53 @@
-import { Center, Image } from "@chakra-ui/react";
+import {
+  Center,
+  IconButton,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { InfoOutlineIcon } from "@chakra-ui/icons";
+import { BaseButton } from "../parts/BaseButton";
+import { Context } from "../../store";
+import { useContext } from "react";
 
-export const Header = () => (
-  <Center bg="choco.500" height="48px" width="100%" justifyContent="center">
-    <Image src="/title.png" height="24px" />
-  </Center>
-);
+export const Header = () => {
+  const { total } = useContext(Context);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Center bg="choco.500" height="48px" width="100%" justifyContent="center">
+        <Spacer />
+        <Image src="/title.png" height="24px" />
+        <Spacer />
+        <IconButton
+          aria-label="Info"
+          icon={<InfoOutlineIcon />}
+          variant="ghost"
+          color="white"
+          colorScheme="blackAlpha"
+          mr="8px"
+          onClick={onOpen}
+        />
+      </Center>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>これまであなたが集めたお菓子の数は {total}コ！</ModalBody>
+          <ModalFooter>
+            <BaseButton text="　OK　" onClick={onClose} />
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
